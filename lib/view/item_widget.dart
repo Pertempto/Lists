c16bc54da4c0b7a1f8f91fd6a8c0a8f5a5573a43
tycebrown <tycebrown247@gmail.com>
2023-02-08 07:42:37 -0600

    Removed the extra build methods this time :)

--- lib/view/item_widget.dart
+++ lib/view/item_widget.dart
@@ -28,20 +28,19 @@ class _ItemWidgetState extends State<ItemWidget> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-          title: _buildEditDialogTitle(context),
-          content: _buildEditDialogContent(context),
-          actions: [_buildSubmitButton(context)]),
-    );
-  }
-
-  Widget _buildEditDialogTitle(BuildContext context) =>
-      const Text("Enter Item", textAlign: TextAlign.center);
-
-  Widget _buildEditDialogContent(BuildContext context) {
-    return TextFormField(
-      controller: _textEditingController,
-      autofocus: true,
-      onFieldSubmitted: (_) => _submitNewItemValue(context: context),
+        title: const Text("Enter Item", textAlign: TextAlign.center),
+        content: TextFormField(
+          controller: _textEditingController,
+          autofocus: true,
+          onFieldSubmitted: (_) => _submitNewItemValue(context: context),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () => _submitNewItemValue(context: context),
+            child: const Text('Submit'),
+          )
+        ],
+      ),
     );
   }
 
@@ -52,8 +51,4 @@ class _ItemWidgetState extends State<ItemWidget> {
 
   void updateItemValue(String newValue) =>
       setState(() => widget.item.value = newValue);
-
-  ElevatedButton _buildSubmitButton(BuildContext context) => ElevatedButton(
-      onPressed: () => _submitNewItemValue(context: context),
-      child: const Text('Submit'));
 }
