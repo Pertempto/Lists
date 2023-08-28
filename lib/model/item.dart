ca3da129267c5dae53ae930b5e8fb9df9238763f
Pertempto <pertemptodisce@gmail.com>
2023-08-28 05:07:07 -0400

    Set default item type to checkbox.

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -32,7 +32,7 @@ class Item {
   @ignore
   bool get isScheduled => scheduling != null;
 
-  Item([this.value = '', this.itemType = ItemType.text, this._scheduling]);
+  Item([this.value = '', this.itemType = ItemType.checkbox, this._scheduling]);
 
   // This method is needed to update the fields of a cached `Item`
   // in the `IsarLinks` of a `ListModel` with the same `id` as `this`,
