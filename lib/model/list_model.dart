fbfc5fa60023004041dd3ac7974fbda2a02d22be
tycebrown <91034018+tycebrown@users.noreply.github.com>
2023-09-09 19:22:13 -0500

    Removed an unneccessary space in asMarkdown

--- lib/model/list_model.dart
+++ lib/model/list_model.dart
@@ -116,7 +116,7 @@ class ListModel {
 
   String asMarkdown() => ['## $title'].followedBy(items.map((item) {
         final leading = (item.itemType == ItemType.checkbox
-            ? '- [${item.isChecked ? 'x' : ' '}] '
+            ? '- [${item.isChecked ? 'x' : ' '}]'
             : '-');
         return '$leading ${item.value}';
       })).join('\n');
